be1b62242a4dbbc1d1d30a94c7764a13b1268cb7
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-11-17 17:28:17 +0100

    Increment version; highlight auto-ungzipping functionality in examples

--- src/ocp.go
+++ src/ocp.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	version   = "2.2"
+	version   = "2.3"
 	useragent = "Optimus Cache Prime/" + version + " (http://patrickmylund.com/projects/ocp/)"
 )
 
@@ -196,7 +196,7 @@ func main() {
 		fmt.Println("Examples:")
 		fmt.Println(" ", os.Args[0], "sitemap.xml")
 		fmt.Println(" ", os.Args[0], "http://mysite.com/sitemap.xml")
-		fmt.Println(" ", os.Args[0], "-c 10 http://mysite.com/sitemap.xml")
+		fmt.Println(" ", os.Args[0], "-c 10 http://mysite.com/sitemap.xml.gz")
 		fmt.Println(" ", os.Args[0], "-l /var/www/mysite.com/wp-content/cache/supercache/ http://mysite.com/sitemap.xml")
 		fmt.Println(" ", os.Args[0], "-l /var/www/mysite.com/wp-content/w3tc/pgcache/ -ls _index.html http://mysite.com/sitemap.xml")
 		fmt.Println("")
